4cc516f30afb469210de1f49b8693fc77568bb2c
Betruck Solomon <ouma.solomon22@students.dkut.ac.ke>
2024-10-03 15:57:17 +0300

    CORS

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,11 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const authRoutes = require('./routes/authRoutes');
 
+const corsOptions = {
+  origin: 'https://donashion.netlify.app', // Replace this with your frontend URL
+  optionsSuccessStatus: 200
+};
+
 dotenv.config();
 const app = express();
 
@@ -12,7 +17,7 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Enable CORS
-app.use(cors());
+app.use(cors(corsOptions));
 
 // Routes
 app.use('/api/auth', authRoutes);
